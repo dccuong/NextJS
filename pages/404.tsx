ba8668e257f5eec459768f408548dba7a2bcea0a
cuongndph18057 <cuongksv10@gmail.com>
2022-07-14 13:18:54 +0700

    first commit

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+type Props = {}
+
+const Layout404 = (props: Props) => {
+  return (
+    <div><img src="Home" alt="" /></div>
+  )
+}
+Layout404.Layout = Layout404
+export default Layout404

86b07b6a960a5ec3af053a44db0a2bfb8e1412fd
cuongndph18057 <cuongksv10@gmail.com>
2022-07-16 05:30:57 +0700

    new

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,31 +1,51 @@
 import React from 'react'
 import { GetStaticProps, GetStaticPropsContext } from "next"
+import useSWR from 'swr'
 import Link from 'next/link'
 
 type ProductProps = {
   products: any[]
 }
 
+
+
 const Products = ({ products }: ProductProps) => {
+  const url = `https://61e7aa5ae32cd90017acbc44.mockapi.io/post`;
+
+  // lấy dữ liệu từ api
+  const fetcher = async (url) => await (await fetch(url)).json();
+  const { data, error } = useSWR(url, fetcher, { dedupingInterval: 3000 })
+
+  if (!data) return <div>Loading...</div>
+  if (error) return <div>Failed to load</div>
+
+
   console.log('Product component', products)
-  if (!products) return null;
+  if (!data) return null;
   return (
-    <div>{products.map(item => (
+    <div>{data.map(item => (
       <div key={item.id}><Link href={`/products/${item.id}`}>{item.title}</Link></div>
     ))}</div>
   )
 }
 
-export const getStaticProps: GetStaticProps<ProductProps> = async (context: GetStaticPropsContext) => {
-  console.log('getStaticProps');
-  const response = await fetch(`https://61e7aa5ae32cd90017acbc44.mockapi.io/post`);
-  const data = await response.json();
+// export const getStaticProps: GetStaticProps<ProductProps> = async (context: GetStaticPropsContext) => {
+//   console.log('getStaticProps');
+//   const response = await fetch(`https://61e7aa5ae32cd90017acbc44.mockapi.io/post`);
+//   const data = await response.json();
 
-  return {
-    props: {
-      products: data.map(item => ({ id: item.id, title: item.title }))
-    },
-    revalidate: 6
-  }
-}
+//   return {
+//     props: {
+//       products: data.map(item => ({ id: item.id, title: item.title }))
+//     },
+//     revalidate: 6
+//   }
+// }
+// getStaticProps without data
+// getStaticProps with data
+
+// SSR - Server Side rendering
+// SSG - Sever site generation
+// ISG - incremental site generation
+// CSR - Client Side Rendering
 export default Products
